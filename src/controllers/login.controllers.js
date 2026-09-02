2dd077e593f88b48b4901bedcab3a4b9a1cf4494
Pratyush Kumar <pratyushgurjar07@gmail.com>
2026-09-02 19:11:34 +0530

    login controller

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -1,10 +1,19 @@
 
+import { apiErrors } from "../utils/ApiErrors.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { apiResponse } from "../utils/apiResponse.js";
 
+// req body -> data
+// username or email
+// find the user
+// check password
+// generate access token and refresh token
+// send cookie
+ 
 const loginUser= asyncHandler(async (req,res)=>{
-    res.status(200).json({
-        message:"ok"
-    })
+    
 })
 
 export {loginUser};
